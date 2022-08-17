0d7299fc935fe4311ca3f20394e447bffd1f86a2
Jobsidney <jobsidney67@gmail.com>
2022-08-18 02:45:57 +0300

    commit tree 510054dad61360691d9e45668d9b8654db694628

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  # code an argument here
   # Your code here
+  dwarves.map.with_index{
+    |name,index|
+    puts("#{index +1}. #{name}")
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planet)# code an argument here
   # Your code here
+  planet.map{
+    |word|
+    "#{word.capitalize}!"
+  }
 end
+p summon_captain_planet(planeteer_calls)
 
-def find_the_cheese# code an argument here
+
+assorted_words = ["two", "go","industrious", "bop"]
+def long_planeteer_calls(planeteer)# code an argument here
+  # Your code here
+  planeteer.find{|word| word.length>4} ? true : false
+end
+p long_planeteer_calls(assorted_words)
+
+def find_the_cheese(strArray)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+  strArray.find{
+    |cheese|
+    if cheese_types.include?(cheese)
+      cheese
+    else
+      nil
+    end
+  }
 end
+p find_the_cheese(["crackers", "gouda", "thyme"])
